42aae676d0b58dddd1b2f6a23226f2d42e62879c
Hang Yuan <JohannYuan@gmail.com>
2021-06-04 18:08:07 -0500

    fix orientation in dihedral build.

--- src/python/object_in_fluid/oif_classes_new.py
+++ src/python/object_in_fluid/oif_classes_new.py
@@ -15,17 +15,24 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import espressomd
+from espressomd.interactions import HarmonicBond, Dihedral
 import numpy as np
+from scipy.spatial import ConvexHull
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import ipdb
 
 class Mesh:
     '''
     mesh data of elastic objects
     '''
-    def __init__(self):
+    def __init__(self, nodes_file="", triangles_file="", rescale=(1.0, 1.0, 1.0)):
         # input files
-        self.nodes_file = ""
-        self.triangles_file = ""
+        self.nodes_file = nodes_file
+        self.triangles_file = triangles_file
+
+        # scaling factors
+        self.rescale = rescale
 
         # mesh data
         self.vertices = []
@@ -43,6 +50,82 @@ class Mesh:
         self.faces_vertices2index = {}
         self.dihedrals_vertices2index = {}
 
+        # mesh properties
+        self.avg_edge_length = None
+        self.total_area = None
+        self.total_volume = None
+
+    def initialize(self):
+        '''
+        initialize mesh data
+        '''
+
+        # load input data
+
+        if nodes_file == "":
+            raise Exception("Missing nodes file. Abort.")
+        self.load_nodes(self.nodes_file)
+
+        if self.triangles_file == "":
+            # construct triangulation according to input nodes
+            self.build_faces()
+        else:
+            # load provided triangulation
+            self.load_triangles(self.triangles_file)
+
+        # build additional info
+        self.build_edges()
+        self.build_dihedrals()
+
+        for dihedral in self.dihedrals:
+            dihedral.get_dihedral_angle()
+
+        #self.plot_points()
+
+    def plot_points(self):
+        fig = plt.figure()
+        ax = Axes3D(fig)
+        #vertices = np.zeros( (self.vertices_num, 3) )
+        #for i in range(self.vertices_num):
+        #    vertices[i][0] = self.vertices[i].x
+        #    vertices[i][1] = self.vertices[i].y
+        #    vertices[i][2] = self.vertices[i].z
+        #ax.scatter(vertices[:,0], vertices[:,1], vertices[:,2])
+        dihedral = self.dihedrals[0]
+        print(dihedral.index)
+        r1 = dihedral.r1
+        r2 = dihedral.r2
+        r3 = dihedral.r3
+        r4 = dihedral.r4
+
+        ax.scatter(dihedral.r1[0], dihedral.r1[1], dihedral.r1[2])
+        ax.scatter(dihedral.r2[0], dihedral.r2[1], dihedral.r2[2])
+        ax.scatter(dihedral.r3[0], dihedral.r3[1], dihedral.r3[2])
+        ax.scatter(dihedral.r4[0], dihedral.r4[1], dihedral.r4[2])
+
+        ax.text(dihedral.r1[0], dihedral.r1[1], dihedral.r1[2], '1: {}'.format(dihedral.v1) )
+        ax.text(dihedral.r2[0], dihedral.r2[1], dihedral.r2[2], '2: {}'.format(dihedral.v2) )
+        ax.text(dihedral.r3[0], dihedral.r3[1], dihedral.r3[2], '3: {}'.format(dihedral.v3) )
+        ax.text(dihedral.r4[0], dihedral.r4[1], dihedral.r4[2], '4: {}'.format(dihedral.v4) )
+
+        ax.plot([r1[0], r2[0]],[r1[1], r2[1]],[r1[2], r2[2]])
+        ax.plot([r2[0], r3[0]],[r2[1], r3[1]],[r2[2], r3[2]], ':')
+        ax.plot([r3[0], r1[0]],[r3[1], r1[1]],[r3[2], r1[2]])
+        ax.plot([r3[0], r4[0]],[r3[1], r4[1]],[r3[2], r4[2]], '--')
+        plt.show()
+
+    def compute_mesh_properties(self):
+        self.compute_average_edge_length()
+
+    def compute_average_edge_length(self):
+        '''
+        average edge length
+        '''
+        self.avg_edge_length = 0.0
+        for edge in self.edges:
+            self.avg_edge_length += edge.get_length()
+        self.avg_edge_length /= self.edges_num
+
     def load_nodes(self, filename):
         '''
         load cooridnates of each vertex
@@ -54,9 +137,9 @@ class Mesh:
                 data = line.split()
 
                 index = int(data[0])
-                x = float(data[1])
-                y = float(data[2])
-                z = float(data[3])
+                x = self.rescale[0] * float(data[1])
+                y = self.rescale[1] * float(data[2])
+                z = self.rescale[2] * float(data[3])
 
                 self.vertices.append(Vertex(index, float(x), float(y), float(z)))
 
@@ -88,10 +171,12 @@ class Mesh:
 
                 if ort == 1:
                     newFace = Face(index, v1, v2, v3)
+                    newFace.set_vertices_pos(r1, r2, r3)
                     self.faces.append(newFace)
                     self.faces_vertices2index.update({newFace.vertices:index})
                 else:
                     newFace = Face(index, v3, v2, v1)
+                    newFace.set_vertices_pos(r3, r2, r1)
                     self.faces.append(newFace)
                     self.faces_vertices2index.update({newFace.vertices:index})
 
@@ -103,26 +188,30 @@ class Mesh:
         '''
         build edges according to faces
         '''
-        edges = set()
+        edges_set = set()
 
         for face in self.faces:
             edge1 = frozenset([face.v1, face.v2])
             edge2 = frozenset([face.v2, face.v3])
             edge3 = frozenset([face.v3, face.v1])
 
-            edges.add(edge1)
-            edges.add(edge2)
-            edges.add(edge3)
+            edges_set.add(edge1)
+            edges_set.add(edge2)
+            edges_set.add(edge3)
 
         self.edges_num = 0
 
-        for edge in edges:
+        for edge in edges_set:
             index = self.edges_num
             vertex_pair = [vertex for vertex in edge]
             v1 = vertex_pair[0]
             v2 = vertex_pair[1]
 
+            r1 = self.vertices[v1].pos
+            r2 = self.vertices[v2].pos
+
             newEdge = Edge(index, v1, v2)
+            newEdge.set_vertices_pos(r1, r2)
             self.edges.append(newEdge)
             self.edges_vertices2index.update({newEdge.vertices:index})
 
@@ -130,36 +219,151 @@ class Mesh:
 
         print('{} edges added.'.format(self.edges_num))
 
+    def build_faces(self):
+        '''
+        use ConvexHull to do triangulation of surface
+        '''
+        vertices = np.zeros( (self.vertices_num, 3) )
+        for i in range(self.vertices_num):
+            vertices[i][0] = self.vertices[i].x
+            vertices[i][1] = self.vertices[i].y
+            vertices[i][2] = self.vertices[i].z
+        ConvexSurface = ConvexHull(vertices)
+        surface_tri = ConvexSurface.simplices
+
+        self.faces_num = surface_tri.shape[0]
+        assert surface_tri.shape[1] == 3
+
+        for i in range(self.faces_num):
+            index = i
+            v1 = surface_tri[i][0]
+            v2 = surface_tri[i][1]
+            v3 = surface_tri[i][2]
+
+            r1 = vertices[v1]
+            r2 = vertices[v2]
+            r3 = vertices[v3]
+
+            ort = int(np.sign( np.dot(r1, np.cross(r2, r3)) ))
+            if ort == 1:
+                newFace = Face(index, v1, v2, v3)
+                newFace.set_vertices_pos(r1, r2, r3)
+                self.faces.append(newFace)
+                self.faces_vertices2index.update({newFace.vertices:index})
+            else:
+                newFace = Face(index, v3, v2, v1)
+                newFace.set_vertices_pos(r3, r2, r1)
+                self.faces.append(newFace)
+                self.faces_vertices2index.update({newFace.vertices:index})
+
+        print('{} faces added.'.format(self.faces_num))
+
     def build_dihedrals(self):
         '''
-        build dihedrals according to edges
+        build dihedrals according to faces
         '''
         self.dihedrals_num = 0
+        dihedrals_set = set()
 
-        for edge in self.edges:
-            edge_vertices = edge.vertices
+        for face in self.faces:
+            # first common edge (v1 -> v2)
+            edge1 = frozenset([face.v1, face.v2])
 
-            # find two faces shared this edge
-            adjacent_faces = []
-            for face in self.faces:
-                if edge_vertices.issubset(face.vertices):
-                    adjacent_faces.append(face)
-            assert len(adjacent_faces) == 2
+            adjacent_face = None
+            for face_tmp in self.faces:
+                if edge1.issubset(face_tmp.vertices) and (face_tmp.index != face.index):
+                    adjacent_face = face_tmp
+                    break
+            if adjacent_face == None:
+                raise Exception("Cannot find the adjacent triangle.")
 
-            face1 = adjacent_faces[0]
-            face2 = adjacent_faces[1]
+            # v3 -> (v1 -> v2) -> v4
+            v1 = face.v3
+            v2 = face.v1
+            v3 = face.v2
+            v4 = next(iter(adjacent_face.vertices.difference(edge1)))
+            dihedral_index = frozenset([v1,v2,v3,v4])
+            if dihedral_index not in dihedrals_set:
+                index = self.dihedrals_num
 
-            index = self.dihedrals_num
-            v1 = next(iter(face1.vertices.difference(edge_vertices)))
-            v2 = edge.v1
-            v3 = edge.v2
-            v4 = next(iter(face2.vertices.difference(edge_vertices)))
+                r1 = self.vertices[v1].pos
+                r2 = self.vertices[v2].pos
+                r3 = self.vertices[v3].pos
+                r4 = self.vertices[v4].pos
 
-            newDihedral = Dihedral(index, v1, v2, v3, v4)
-            self.dihedrals.append(newDihedral)
-            self.dihedrals_vertices2index.update({newDihedral.vertices:index})
+                newDihedral = Dihedral(index, v1, v2, v3, v4)
+                newDihedral.set_vertices_pos(r1, r2, r3, r4)
+                self.dihedrals.append(newDihedral)
+                self.dihedrals_vertices2index.update({newDihedral.vertices:index})
 
-            self.dihedrals_num += 1
+                self.dihedrals_num += 1
+                dihedrals_set.add(dihedral_index)
+
+            # second common edge (v2 -> v3)
+            edge2 = frozenset([face.v2, face.v3])
+
+            adjacent_face = None
+            for face_tmp in self.faces:
+                if edge2.issubset(face_tmp.vertices) and (face_tmp.index != face.index):
+                    adjacent_face = face_tmp
+                    break
+            if adjacent_face == None:
+                raise Exception("Cannot find the adjacent triangle.")
+
+            # v1 -> (v2 -> v3) -> v4
+            v1 = face.v1
+            v2 = face.v2
+            v3 = face.v3
+            v4 = next(iter(adjacent_face.vertices.difference(edge2)))
+            dihedral_index = frozenset([v1,v2,v3,v4])
+            if dihedral_index not in dihedrals_set:
+                index = self.dihedrals_num
+
+                r1 = self.vertices[v1].pos
+                r2 = self.vertices[v2].pos
+                r3 = self.vertices[v3].pos
+                r4 = self.vertices[v4].pos
+
+                newDihedral = Dihedral(index, v1, v2, v3, v4)
+                newDihedral.set_vertices_pos(r1, r2, r3, r4)
+                self.dihedrals.append(newDihedral)
+                self.dihedrals_vertices2index.update({newDihedral.vertices:index})
+
+                self.dihedrals_num += 1
+                dihedrals_set.add(dihedral_index)
+
+            # third common edge (v3 -> v1)
+            edge3 = frozenset([face.v3, face.v1])
+
+            adjacent_face = None
+            for face_tmp in self.faces:
+                if edge3.issubset(face_tmp.vertices) and (face_tmp.index != face.index):
+                    adjacent_face = face_tmp
+                    break
+            if adjacent_face == None:
+                raise Exception("Cannot find the adjacent triangle.")
+
+            # v2 -> (v3 -> v1) -> v4
+            v1 = face.v2
+            v2 = face.v3
+            v3 = face.v1
+            v4 = next(iter(adjacent_face.vertices.difference(edge3)))
+            dihedral_index = frozenset([v1,v2,v3,v4])
+            if dihedral_index not in dihedrals_set:
+                index = self.dihedrals_num
+
+                r1 = self.vertices[v1].pos
+                r2 = self.vertices[v2].pos
+                r3 = self.vertices[v3].pos
+                r4 = self.vertices[v4].pos
+
+                newDihedral = Dihedral(index, v1, v2, v3, v4)
+                newDihedral.set_vertices_pos(r1, r2, r3, r4)
+                self.dihedrals.append(newDihedral)
+                self.dihedrals_vertices2index.update({newDihedral.vertices:index})
+
+                self.dihedrals_num += 1
+                dihedrals_set.add(dihedral_index)
 
         print('{} dihedrals added.'.format(self.dihedrals_num))
 
@@ -188,6 +392,19 @@ class Edge:
         self.v2 = v2
         self.vertices = frozenset([v1, v2])
 
+        self.r1 = None
+        self.r2 = None
+
+    def set_vertices_pos(self, r1, r2):
+        self.r1 = r1
+        self.r2 = r2
+
+    def get_length(self):
+        '''
+        return edge length
+        '''
+        return np.linalg.norm(self.r1 - self.r2)
+
 class Face:
     '''
     represent a face data (2D)
@@ -198,6 +415,15 @@ class Face:
         self.v2 = v2
         self.v3 = v3
         self.vertices = frozenset([v1, v2, v3])
+
+        self.r1 = None
+        self.r2 = None
+        self.r3 = None
+
+    def set_vertices_pos(self, r1, r2, r3):
+        self.r1 = r1
+        self.r2 = r2
+        self.r3 = r3
 
 class Dihedral:
     '''
@@ -211,13 +437,135 @@ class Dihedral:
         self.v4 = v4
         self.vertices = frozenset([v1, v2, v3, v4])
 
+        self.r1 = None
+        self.r2 = None
+        self.r3 = None
+        self.r4 = None
+
+    def set_vertices_pos(self, r1, r2, r3, r4):
+        self.r1 = r1
+        self.r2 = r2
+        self.r3 = r3
+        self.r4 = r4
+
+    def get_dihedral_angle(self):
+        '''
+        return dihedral angle
+
+        following espresso convention
+
+        v1 -> (v2 -> v3) -> v4
+        '''
+        r12 = self.r2 - self.r1
+        r23 = self.r3 - self.r2
+        r34 = self.r4 - self.r3
+
+        n1 = np.cross(r12, r23)
+        n2 = np.cross(r23, r34)
+
+        n1_norm = np.linalg.norm(n1)
+        n2_norm = np.linalg.norm(n2)
+
+        n1_hat = n1 / n1_norm
+        n2_hat = n2 / n2_norm
+
+        cosphi = np.dot(n1_hat, n2_hat)
+
+        phi = np.arccos(cosphi)
+
+        return phi
+
+class ElasticObjectType:
+    '''
+    Define properties of an elastic object
+    '''
+    def __init__(self, ks_A, ks_B, kb_A, kb_B, nodes_file, triangles_file,
+            rescale=(1.0, 1.0, 1.0)):
+        self.mesh = Mesh(nodes_file, triangles_file, rescale)
+
+        # elastic parameters
+        self.ks_A = ks_A
+        self.ks_B = ks_B
+
+        self.kb_A = kb_A
+        self.kb_B = kb_B
+
+        # bonded interactions
+        self.stretching_interactions = []
+        self.bending_interactions = []
+        self.local_area_conservation = []
+        self.global_area_conservation = []
+        self.global_volume_conservation = []
+
+    def define_bonded_interactions(self):
+        '''
+        setup all bonded interactions
+        '''
+        pass
+
+    def define_stretching_interactions(self):
+        '''
+        define stretching interactions for each edge
+        '''
+        # stretching
+        for edge in self.mesh.edges:
+            if edge.type == 0:
+                # AA-bond
+                ks_AA = self.ks_A / 2.0
+                l0 = self.mesh.avg_edge_length
+                hb_AA = HarmonicBond(k=ks_AA, r_0=l0, r_cut=2*l0)
+                self.stretching_interactions.append( (hb_AA, [v1,v2]) )
+            elif edge.type == 1:
+                # BB-bond
+                ks_BB = self.ks_B / 2.0
+                l0 = self.mesh.avg_edge_length
+                hb_BB = HarmonicBond(k=ks_BB, r_0=l0, r_cut=2*l0)
+                self.stretching_interactions.append( (hb_BB, [v1,v2]) )
+            elif edge.type == 2:
+                # AB-bond
+                ks_AB = self.ks_A*self.ks_B / (self.ks_A + self.ks_B)
+                l0 = self.mesh.avg_edge_length
+                hb_AB = HarmonicBond(k=ks_AB, r_0=l0, r_cut=2*l0)
+                self.stretching_interactions.append( (hb_AB, [v1,v2]) )
+            else:
+                raise Exception("Unrecognized edge type.")
+
+    def define_bending_interactions(self):
+        '''
+        define bending interactions for each dihedral
+        '''
+        # bending
+        for dihedral in self.mesh.dihedrals:
+            if dihedral.type == 0:
+                # AA-dihedral
+                kb_AA = self.kb_A
+                v1 = dihedral.v1
+                v2 = dihedral.v2
+                v3 = dihedral.v3
+                v4 = dihedral.v4
+
+                r1 = self.mesh.vertices[v1].pos
+                r2 = self.mesh.vertices[v2].pos
+                r3 = self.mesh.vertices[v3].pos
+                r4 = self.mesh.vertices[v4].pos
+
+                phi0 = np.pi
+                #phi0 = dihedral.get_dihedral_angle()
+
+                dihedral_AA = Dihedral(bend=kb_AA, mult=1, phase=phi0)
+                self.bending_interactions.append( (dihedral_AA, [v1,v2,v3,v4]) )
+
+
+class ElasticObject:
+    '''
+    represent an elastic object
+    '''
+    pass
+
 if __name__ == "__main__":
     # test purpose
-    nodesfile = '1082.6.6.nodes'
-    trianglesfile = '1082.6.6.triangles'
-    testMesh = Mesh()
-    testMesh.load_nodes(nodesfile)
-    testMesh.load_triangles(trianglesfile)
-    testMesh.build_edges()
-    testMesh.build_dihedrals()
+    nodes_file = '1082.6.6.nodes'
+    triangles_file = '1082.6.6.triangles'
+    testMesh = Mesh(nodes_file, triangles_file)
+    testMesh.initialize()
     ipdb.set_trace()
